67e243fe2101375d3324eabcc51b71423d4c51a4
vennela2325 <vennelakothoju@gmail.com>
2024-12-13 23:49:19 +0530

    operators task is done

---
+++ operators.js
@@ -0,0 +1,54 @@
+a=20;
+a=30;
+a=a+70;
+a=a+100;
+a+=200;
+console.log(a);
+var b=20;
+b=10;
+b=10-2;
+console.log(b);
+var c=100;
+c=c*2;
+c*=1;
+console.log(c);
+var a=10;
+var b=20;
+console.log(a>b && b<a);
+console.log(a>b && a<b);
+console.log(false && true);
+var a=10;
+var b=0;
+console.log(b && a);
+var a=10;
+var b=0;
+console.log(a>b || b>a);
+console.log(a<b || b>a);
+var a=10;
+var b=0;
+var c=10;
+console.log(a<b || a<b ||c<b);
+var a=10;
+console.log(!a);
+console.log(!!a);
+if(!!"vamsi"){
+    alert("good morning");
+}
+else{
+    alert("hlo guys");
+}
+if([]&&[])
+{
+    alert("good morning");
+}
+else
+{
+    alert("hlo guys");
+}
+if(["vamsi"]&&[1,2,3])
+    {
+    alert("good morning");
+}
+else{
+    alert("hlo guys");
+}
